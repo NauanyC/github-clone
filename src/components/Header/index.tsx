14112e4191ac3e8f5b369f645999d448c9c6be1c
Nauany Cardoso <nauanycc@outlook.com.br>
2022-01-11 23:16:24 -0300

    feat: add/style navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
-
+import { Container, GithubLogo, SearchForm } from './styles';
 
 const Header: React.FC = () => {
-    return <h1>Header</h1>
-}
+  return (
+    <Container>
+      <GithubLogo />
+      <SearchForm>
+        <input placeholder="Enter your Username or Repository..."></input>
+      </SearchForm>
+    </Container>
+  );
+};
 
 export default Header;
