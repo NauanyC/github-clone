884f3039a9e0186a8b11344c8c069199d3d8b39b
Nauany Cardoso <nauanycc@outlook.com.br>
2022-01-12 14:01:29 -0300

    feat: add icons

--- src/components/ProfileData/styles.ts
+++ src/components/ProfileData/styles.ts
@@ -1,26 +1,37 @@
-import styled from 'styled-components';
-
-interface AvatarProps {
-  src: string;
-  alt: string;
-}
+import styled, { css } from 'styled-components';
+import {
+  RiGroupLine,
+  RiBuilding4Line,
+  RiMapPin2Line,
+  RiMailLine,
+  RiLinksLine,
+} from 'react-icons/ri';
 
 export const Container = styled.div``;
 
 export const Flex = styled.div``;
 
-export const Avatar = styled.div<AvatarProps>``;
+export const Avatar = styled.img`
+  width: 100%;
+`;
 
 export const Row = styled.div``;
 
 export const Column = styled.div``;
 
-export const PeopleIcon = styled.div``;
+export const iconsCSS = css`
+  width: 16px;
+  height: 16px;
+  fill: var(--icon);
+  flex-shrink: 0;
+`;
 
-export const CompanyIcon = styled.div``;
+export const PeopleIcon = styled(RiGroupLine)``;
 
-export const LocationIcon = styled.div``;
+export const CompanyIcon = styled(RiBuilding4Line)``;
 
-export const EmailIcon = styled.div``;
+export const LocationIcon = styled(RiMapPin2Line)``;
 
-export const BlogIcon = styled.div``;
+export const EmailIcon = styled(RiMailLine)``;
+
+export const BlogIcon = styled(RiLinksLine)``;
