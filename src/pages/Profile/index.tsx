0f4a3f6768e3ea91b174c586206c347057cb440b
Nauany Cardoso <nauanycc@outlook.com.br>
2022-01-16 19:28:58 -0300

    feat: resize top tabs line

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -47,7 +47,10 @@ const Profile: React.FC = () => {
   return (
     <Container>
       <Tab className="desktop">
-        <TabContent />
+        <div className="wrapper">
+          <span className="offset" />
+          <TabContent />
+        </div>
         <span className="line" />
       </Tab>
       <Main>
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -109,6 +109,17 @@ export const Tab = styled.div`
 
     @media (min-width: 768px) {
       display: unset;
+
+      .wrapper {
+        display: flex;
+        margin: 0 auto;
+        max-width: 1280px;
+      }
+
+      .offset {
+        width: 24%;
+        margin-right: var(--horizontalPadding);
+      }
     }
   }
 `;
