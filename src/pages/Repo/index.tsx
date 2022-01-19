74eff03e4263cf90208ca04f6c1697008fd638a6
Nauany Cardoso <nauanycc@outlook.com.br>
2022-01-18 23:21:33 -0300

    feat: add repo page

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,8 +1,48 @@
 import * as React from 'react';
-
+import { Link } from 'react-router-dom';
+import {
+  BreadCrumb,
+  Stats,
+  RepoIcon,
+  Container,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles';
 
 const Repo: React.FC = () => {
-    return <h1>Repo</h1>
-}
+  return (
+    <Container>
+      <BreadCrumb>
+        <RepoIcon />
+        <Link className={'username'} to="/NauanyC">
+          NauanyC
+        </Link>
+        <span>/</span>
+        <Link className={'reponame'} to="/NauanyC/github-clone">
+          github-clone
+        </Link>
+      </BreadCrumb>
+      <p>Contains a github clone.</p>
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>12</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+      <LinkButton href="https://github.com/NauanyC/github-clone">
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  
-`;
+export const Container = styled.div``;
+
+export const BreadCrumb = styled.div``;
+
+export const Stats = styled.div``;
+
+export const RepoIcon = styled.div``;
+
+export const StarIcon = styled.div``;
+
+export const ForkIcon = styled.div``;
+
+export const LinkButton = styled.a``;
+
+export const GithubIcon = styled.div``;
