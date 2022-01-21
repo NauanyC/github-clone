21c771965ba81ea1443241f1c90b51b93c1758a6
Nauany Cardoso <nauanycc@outlook.com.br>
2022-01-21 19:44:51 -0300

    feat: style breadcrumb

--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -1,12 +1,50 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { RiBookMarkLine, RiStarLine } from 'react-icons/ri';
 import { AiOutlineFork } from 'react-icons/ai';
 import { FaGithub } from 'react-icons/fa';
 import { iconsCSS } from '../../assets/styles/icons';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
 
-export const BreadCrumb = styled.div``;
+  > p {
+    font-size: 16px;
+  }
+`;
+
+export const BreadCrumb = styled.div`
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  white-space: nowrap;
+
+  font-size: 18px;
+
+  > a {
+    color: var(--link);
+    text-decoration: none;
+
+    &::hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
+
+  &.username {
+    margin-left: 8px;
+  }
+
+  &.reponame {
+    font-weight: 600;
+  }
+
+  > span {
+    padding: 0 5px;
+  }
+`;
 
 export const Stats = styled.div``;
 
